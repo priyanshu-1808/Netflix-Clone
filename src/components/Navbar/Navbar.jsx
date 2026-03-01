cd3b9d97b3d91b51b1fd11cba2a214c9f609d571
priyanshu-1808 <kr.priyanshu.1808@gmail.com>
2026-03-01 10:05:17 +0530

    Initial commit

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import './Navbar.css'
+import logo from '../../assets/logo.png'
+import search_icon from '../../assets/search_icon.svg'
+import bell_icon from '../../assets/bell_icon.svg'
+import profile_img from '../../assets/profile_img.png'
+import caret_icon from '../../assets/caret_icon.svg'
+
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+
+      {/* LEFT */}
+      <div className="navbar-left"> 
+        <img src={logo} alt="logo" />
+
+        <ul>
+          <li>Home</li>
+          <li>Tv Shows</li>
+          <li>Movies</li>
+          <li>New & Popular</li>
+          <li>My Lists</li>
+          <li>Browse By Language</li>
+        </ul>
+      </div>
+
+      {/* RIGHT */}
+      <div className="navbar-right">
+
+        <img src={search_icon} alt="" className='icons' />
+        <p>Children</p>
+        <img src={bell_icon} alt="" className='icons' />
+
+        <div className="navbar-profile">
+          <img src={profile_img} alt="" className='profile' />
+          <img src={caret_icon} alt="" />
+
+          <div className="dropdown">
+            <p>Sign out of Netflix</p>
+          </div>
+        </div>
+
+      </div>
+
+    </div>
+  )
+}
+
+export default Navbar
---
+++ src/components/TitleCards/TitleCards.jsx
@@ -0,0 +1,49 @@
+import React, { useEffect, useRef } from 'react'
+import './TitleCards.css'
+import cards_data from '../../assets/cards/Cards_data'
+
+
+const TitleCards = ({title, category}) => {
+
+  const cardsRef = useRef(null);
+
+  const handleWheel = (event) => {
+    event.preventDefault();
+    cardsRef.current.scrollLeft += event.deltaY;
+  }
+
+  useEffect(() => {
+    const ref = cardsRef.current;
+
+    if (ref) {
+      ref.addEventListener("wheel", handleWheel);
+    }
+
+    return () => {
+      if (ref) {
+        ref.removeEventListener("wheel", handleWheel);
+      }
+    };
+  }, []);
+
+  return (
+    <div className='title-cards'>
+
+      <h2>{title?title:"Popular On Netflix"}</h2>
+
+      <div className="card-list" ref={cardsRef}>
+        {cards_data.map((card, index) => (
+          <div className="card" key={index}>
+            <img src={card.image} alt="" />
+            <p>{card.name}</p>
+          </div>
+        ))}
+        
+      </div>
+      
+
+    </div>
+  )
+}
+
+export default TitleCards;
