a3abd0348866bafc4dfc88d2b13f9f05027eb3d6
priyanshu-1808 <kr.priyanshu.1808@gmail.com>
2026-03-01 10:15:52 +0530

    update login page parts in login.jsx and login.css

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,13 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Login.css'
 import logo from '../../assets/logo.png'
 
 const Login = () => {
+
+const [signState,setSignState] = useState("Sign In")
+
+
+
   return (
     <div className='login'>
       <img src={logo} className='login-logo' alt="" />
       <div className="login-form">
-        <h1>Sign Up</h1>
+        <h1>{signState}</h1>
         <form >
           <input type="text" placeholder='Your Name' />
           <input type="email" placeholder='Email' />
