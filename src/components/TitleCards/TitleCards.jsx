8e1179a729b9c9cf1d4e2ecb9f23e833a6ca3633
priyanshu-1808 <kr.priyanshu.1808@gmail.com>
2026-03-13 00:06:50 +0530

    updated TitleCards component

--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -1,11 +1,18 @@
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import './TitleCards.css'
-import cards_data from '../../assets/cards/Cards_data'
 
+const TitleCards = ({ title, category }) => {
 
-const TitleCards = ({title, category}) => {
-
+  const [apiData, setApiData] = useState([]);
   const cardsRef = useRef(null);
+
+  const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzMDRmYzRmZjZkZGZlYjE0YTAwY2NjNzUwNTYyMDg3OSIsIm5iZiI6MTc3MzMzOTI0NS4yOTcsInN1YiI6IjY5YjMwMjZkYWIzMjMzYjc3YTY4NWQ2OCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.9VvzqYFlcHN7jEZ6cx9oW74KqJVtyYmPf3W7BBAKHDw'
+    }
+  };
 
   const handleWheel = (event) => {
     event.preventDefault();
@@ -13,6 +20,12 @@ const TitleCards = ({title, category}) => {
   }
 
   useEffect(() => {
+
+    fetch(`https://api.themoviedb.org/3/movie/${category ? category : "now_playing"}`, options)
+      .then(res => res.json())
+      .then(res => setApiData(res.results))
+      .catch(err => console.error(err));
+
     const ref = cardsRef.current;
 
     if (ref) {
@@ -24,26 +37,35 @@ const TitleCards = ({title, category}) => {
         ref.removeEventListener("wheel", handleWheel);
       }
     };
-  }, []);
+
+  }, [category]);
 
   return (
     <div className='title-cards'>
 
-      <h2>{title?title:"Popular On Netflix"}</h2>
+      <h2>{title ? title : "Popular On Netflix"}</h2>
 
       <div className="card-list" ref={cardsRef}>
-        {cards_data.map((card, index) => (
-          <div className="card" key={index}>
-            <img src={card.image} alt="" />
-            <p>{card.name}</p>
-          </div>
-        ))}
-        
+
+        {apiData.map((card, index) => {
+          return (
+            <div className="card" key={index}>
+
+              <img
+                src={`https://image.tmdb.org/t/p/w500${card.backdrop_path}`}
+                alt={card.original_title}
+              />
+
+              <p>{card.original_title}</p>
+
+            </div>
+          )
+        })}
+
       </div>
-      
 
     </div>
   )
 }
 
-export default TitleCards;
+export default TitleCards
